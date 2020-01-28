1c91db7ee61570fc432296278e9e260ce202b1d4
dzmitriy-melnikov <dimaira2012@yandex.ru>
2020-01-28 15:47:41 +0300

    lesson1 end

--- src/server.js
+++ src/server.js
@@ -3,7 +3,10 @@ import { json, urlencoded } from 'body-parser'
 import morgan from 'morgan'
 import cors from 'cors'
 
+const { log } = console
+
 export const app = express()
+const router = express.Router()
 
 app.disable('x-powered-by')
 
@@ -12,4 +15,48 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-export const start = () => {}
+const logMiddleware = (req, res, next) => {
+  log('logger')
+  next()
+}
+
+app.use(logMiddleware)
+
+/*router
+  .route('/cat')
+  .get()
+  .post()
+  .put()
+  .delete()*/
+
+// app.use('/api', router)
+app.use(router)
+
+router.get('/me', (req, res) => {
+  res.send({
+    me: 'hello'
+  })
+})
+
+app.get('/', (req, res, next) => {
+  res.send({
+    message: `hello world1`
+  })
+  next()
+})
+app.get('/', (req, res) => {
+  res.send({
+    message: `hello world2`
+  })
+})
+
+app.post('/', (req, res) => {
+  log(req.body)
+  res.send({
+    message: 'ok'
+  })
+})
+
+export const start = () => {
+  app.listen(3000, () => log('express listen on localhost:3000'))
+}
